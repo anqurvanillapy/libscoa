0fe37e9d838baf7c5b44d51a5612bf753e86e81e
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-02-09 03:06:36 +0800

    updated cpu.h

--- libscoart/sched/cpu.h
+++ libscoart/sched/cpu.h
@@ -4,8 +4,16 @@
 #include <cstdint>
 #include "sched.h"
 
+/// Count physical/hyper-threading CPUs
 void scoa_cpu_init();
 
+/// Getter of number of CPUs
 uint32_t scoa_cpu_count();
+
+/// Thread assignment to CPUs (pinning)
+uint32_t scoa_cpu_assign(uint32_t count, Scheduler& sched, bool nopin,
+    bool pinaio);
+
+/// TODO: CPU affinity and more.
 
 #endif // !__LIBSCOA_CPU_H
