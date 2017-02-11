5711fc828fe98f69fe942f5020c2bc36d9cb3796
Parallels <zerolocust@gmail.com>
2017-02-11 21:56:03 +0800

    working on match

--- libscoart/actor/match.cc
+++ libscoart/actor/match.cc
@@ -2,23 +2,24 @@
 Matcher::Matcher(const vector<scoa_msg_t &> & msgs_)
 {
     msgs = msgs_;
-    branch_match_success = false;
+    // on_branch_match_success initialize state should be true, because it match nothing
+    branch_match_success = true;
     finished = false;
     cond_index = 0;
+    match_index = 0; 
+    ast_index = 0
 }
 
 Matcher& Matcher::match(string str)
 {
-    if (prematch_pos.empty)
-    {
-        
-    }
+
+
+
 }
 template <typename Element>
 Matcher& Matcher::get_ele(Element ele)
-{
-
-}
+{   
+}   
 template <typename Func>
 Matcher& Matcher::callfunc(Func &lambda)
 {
@@ -26,11 +27,14 @@ Matcher& Matcher::callfunc(Func &lambda)
 }
 Matcher& Matcher::then()
 {
-
+    cond_vec.push_back(std::make_tuple(match_index, branch_match_successed, new vector<uint32_t>(prematch_pos)));
+    return *this;
 }
-Matcher& Matcher:: or ()
+Matcher& Matcher::or()
 {
-
+    auto state = cond_vec.back();
+    std::tie(match_index, branch_match_successed and prematch_pos) = state;
+    return *this;
 }
 bool Matcher::end()
 {
@@ -44,3 +48,4 @@ const std::vector<scoa_msg_t &> msgs;
 std::vector<int> prematch_pos;
 std::vector<int> cond_vec;
 int cond_index;
+uint32_t         match_index;       
--- libscoart/actor/match.h
+++ libscoart/actor/match.h
@@ -1,32 +1,83 @@
 #ifndef __LIBSCOA_ACTOR_MATCH_H
 #define __LIBSCOA_ACTOR_MATCH_H
 #include <vector>
+#include <utility>
+#include <string>
 
 class Matcher
 {
   public:
-    Matcher(const vector<scoa_msg_t&> &);
+    Matcher(const std::vector<scoa_msg_t&> &);
 
-    Matcher& match(string str);
+    Matcher& match(std::string str);
     template <typename Element> Matcher& get_ele(Element ele);
     template <typename Func> Matcher& callfunc(Func& lambda);
     Matcher& then();
     Matcher& or();
+    Matcher& cond_end();
     void end();
 
   private:
-    bool on_branch_match_success;
+    bool branch_match_successed;
     bool finished;
-    std::vector<int> prematch_pos;
-    std::vector<int> cond_vec;
-    int              cond_index;
+    std::vector<BaseAST>  AST_vec;
+    std::vector<uint32_t> prematch_pos;
+    // store <match_index, branch_match_successed and prematch_pos>
+    std::vector<std::tuple<uint32_t, bool, std::vector<int>> cond_vec;
+    uint32_t         cond_index; 
+    uint32_t         match_index;
+    uint32_t         ast_index;    
 };
+
+class BaseAST
+{
+public:
+    virtual ~BastAST(){}
+    virtual void run(Matcher&) = 0;
+
+};
+
+class MatchAST : BaseAST
+{
+public:
+    MatchAST(const std::string& str):str(str) {}
+    void run(Matcher&) override;
+private:
+    std::string str;
+};
+
+class GeteleAST<typename Element> : BaseAST
+{
+public:
+    GeteleAST(Element ele) : ele(ele) {}
+    void run(Matcher&) override;
+private:
+    Element ele;
+};
+
+class CondAST : BaseAST
+{
+public:
+    CondAST(std::unique_ptr<std::vector<BaseAST>> cond) : cond(cond) {}
+    void run(Matcher&) override;
+private:
+    std::unique_ptr<std::vector<BaseAST>> cond;
+};
+
+class CallFuncAST<typename Func>
+{
+public:
+    CallFuncAST(Func& lambda): lambda(lambda) {}
+    void run(Matcher&) override;
+private:
+    Func& lambda;
+}
 
 #endif // __LIBSCOA_ACTOR_MATCH_H
 
     recv()
         .match("ok")
-        .get_ele(&url)
+        .get_ele(&url) -> then(&url)
         .then()
             .match("GET")
             .get_ele(&request)
@@ -38,5 +89,9 @@ class Matcher
             .or()
                 .match("PUT")
                 .callfunc([]() { /* do something */ })
+            .cond_end()
+        .or()
+            .match("DELETE")
+            .callfunc([]() { /* do something */ })
         .end()
 
