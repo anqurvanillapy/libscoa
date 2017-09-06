dfd03d843d4329df81d203a22f59677a9a573f3b
anqurvanillapy <anqurvanillapy@gmail.com>
2017-09-06 13:44:57 +0800

    updated lfq: some interfaces

--- include/libscoart/common.h
+++ include/libscoart/common.h
@@ -1,1 +1,4 @@
 #pragma once
+
+#include <memory>
+#include <atomic>
--- include/libscoart/lfq.h
+++ include/libscoart/lfq.h
@@ -7,6 +7,7 @@ namespace scoa {
 /// lockfreeq is the prototype of message queues and in-/out-boxes that can
 /// atomically enqueue/dequeue.
 
+template <typename T>
 class lockfreeq {
 public:
     lockfreeq() { /* nop */ }
@@ -17,7 +18,38 @@ public:
     lockfreeq& operator=(const lockfreeq&) = delete;
     lockfreeq(lockfreeq&&) = delete;
     lockfreeq& operator=(lockfreeq&&) = delete;
+
+    auto front() const { return head; }
+    auto back() const { return tail; }
+
+    auto
+    pop_back()
+    {
+    }
+
+    void
+    push_back(T elem)
+    {
+    }
+
+    auto
+    pop_front()
+    {
+    }
+
+    void
+    push_front(T elem)
+    {
+    }
 private:
+    struct lfq_node {
+        T t;
+        shared_ptr<lfq_node> next;
+    }
+
+    std::atomic_size_t len{0};
+    std::shared_ptr<lfq_node> head;
+    std::shared_ptr<lfq_node> tail;
 };
 
 } /* namespace scoa */
