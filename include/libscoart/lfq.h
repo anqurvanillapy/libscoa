fdc72f3b4ad6d657cf4852228a4554bfe3f411ac
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-11-05 14:21:45 +0800

    rm third_party

--- include/libscoart/lfq.h
+++ include/libscoart/lfq.h
@@ -17,12 +17,9 @@ protected:
         explicit lfq_node(const T& data) : data(data) { /* nop */ }
     };
 public:
-    lockfreeq()
-        : head_(std::make_shared<lfq_node>()),
-        tail_(head_),
-        dummy_nullsptr_(std::make_shared<lfq_node>())
-    { /* nop */ }
-    virtual ~lockfreeq() { /* nop */ }
+    lockfreeq() : head_(std::make_shared<lfq_node>()), tail_(head_),
+        dummy_node_(std::make_shared<lfq_node>()) { /* nop */ }
+    virtual ~lockfreeq() = default;
 
     // No copy and move.
     lockfreeq(const lockfreeq&) = delete;
@@ -30,42 +27,31 @@ public:
     lockfreeq(lockfreeq&&) = delete;
     lockfreeq& operator=(lockfreeq&&) = delete;
 
-    size_t
-    size() const
-    {
-        return siz_.load(std::memory_order_relaxed);
-    }
+    inline size_t size() const { return siz_.load(); }
 
     void
     push_back(const T& elem)
     {
-        std::shared_ptr<lfq_node> t;
         auto new_node = std::make_shared<lfq_node>(elem);
-
+        std::shared_ptr<lfq_node> t;
         while (true) {
             t = std::atomic_load_explicit(&tail_, std::memory_order_relaxed);
-            hp_[0].store(t.get(), std::memory_order_relaxed);
-            if (t.get() != tail_.get()) continue;
-            if (t->next != nullptr) {
-                std::atomic_compare_exchange_weak_explicit(
-                        &tail_,
-                        &t,
-                        t->next,
+            auto next = t->next;
+            if (t != tail_) continue;
+            if (next != nullptr) {
+                if (std::atomic_compare_exchange_weak_explicit(&tail_, &t, next,
                         std::memory_order_release,
-                        std::memory_order_relaxed);
-                continue;
+                        std::memory_order_relaxed)) continue;
             }
+            if (t != tail_) continue;
             if (std::atomic_compare_exchange_weak_explicit(
                         &t->next,
-                        &dummy_nullsptr_,
+                        &dummy_node_,
                         new_node,
                         std::memory_order_release,
                         std::memory_order_relaxed)) break;
         }
-        std::atomic_compare_exchange_weak_explicit(
-                &tail_,
-                &t,
-                new_node,
+        std::atomic_compare_exchange_weak_explicit(&tail_, &t, new_node,
                 std::memory_order_release,
                 std::memory_order_relaxed);
         siz_.fetch_add(1, std::memory_order_relaxed);
@@ -74,47 +60,32 @@ public:
     T
     pop_front()
     {
-        T ret;
-        std::shared_ptr<lfq_node> h, t;
-
+        T data;
         while (true) {
-            h = std::atomic_load_explicit(&head_, std::memory_order_relaxed);
-            hp_[0].store(h.get(), std::memory_order_relaxed);
-            if (h.get() != head_.get()) continue;
+            auto h = std::atomic_load_explicit(&head_,
+                    std::memory_order_relaxed);
             auto t = std::atomic_load_explicit(&tail_,
                     std::memory_order_relaxed);
             auto next = h->next;
-            hp_[1].store(next.get(), std::memory_order_relaxed);
-            if (h.get() != head_.get()) continue;
+            if (h != head_) continue;
             if (next == nullptr) throw std::runtime_error("empty queue");
-            if (h == std::atomic_load_explicit(&tail_,
-                        std::memory_order_relaxed)) {
-                std::atomic_compare_exchange_weak_explicit(
-                        &tail_,
-                        &t,
-                        next,
+            if (h == t) {
+                if (std::atomic_compare_exchange_weak_explicit(&tail_, &t, next,
                         std::memory_order_release,
-                        std::memory_order_relaxed);
-                continue;
+                        std::memory_order_relaxed)) continue;
             }
-            ret = next->data;
-            if (std::atomic_compare_exchange_weak_explicit(
-                        &head_,
-                        &h,
-                        next,
+            if (h != head_) continue;
+            data = next->data;
+            if (std::atomic_compare_exchange_weak_explicit(&head_, &h, next,
                         std::memory_order_release,
                         std::memory_order_relaxed)) break;
         }
-
         siz_.fetch_sub(1, std::memory_order_relaxed);
-        return ret;
+        return data;
     }
 private:
     std::atomic_size_t siz_{0};
-    std::shared_ptr<lfq_node> head_, tail_, dummy_nullsptr_;
-
-    // Hazard pointers.
-    std::atomic<lfq_node*> hp_[2]{};
+    std::shared_ptr<lfq_node> head_, tail_, dummy_node_;
 };
 
 } /* namespace scoa */
